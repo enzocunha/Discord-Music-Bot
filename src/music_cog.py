3b7dafef96e998e04b00d308a25db38d4f44c2fe
enzocunha <enzcdn@discente.ufg.br>
2023-03-15 15:05:46 -0300

    Adding checks to join and leave

--- src/music_cog.py
+++ src/music_cog.py
@@ -7,21 +7,33 @@ class MusicPlayer(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
         print('Music Player loaded succesfully.')
+        self.YDL_OPTIONS = YDL_OPTIONS = {
+            'format': 'bestaudio', 'noplaylist': 'True'}
+        self.queue = []
 
     @commands.command()
     async def join(self, ctx):
         if ctx.author.voice is None:
-            await ctx.send("You need to be in a voice channel to use this command.")
+            await ctx.send('You need to be in a voice channel to use this command.')
+            return
+
+        if not ctx.voice_client is None:
+            await ctx.send('Already joined a voice channel.')
             return
 
         channel = ctx.author.voice.channel
-        await channel.connect()
-        await ctx.send(f"Connecting to {channel.name}.")
+        self.voice_channel = await channel.connect()
+        await ctx.send(f'Connecting to {channel.name}.')
 
     @commands.command()
     async def leave(self, ctx):
+        if ctx.voice_client is None:
+            await ctx.send('Not connected to any voice channel.')
+            return
+
         await ctx.voice_client.disconnect()
-        await ctx.send(f'Disconnected.')
+        self.voice_channel = None
+        await ctx.send('Disconnected.')
 
     @commands.command()
     async def clear(self, ctx, amount=100):
@@ -33,17 +45,16 @@ class MusicPlayer(commands.Cog):
             await ctx.send("You need to be in a voice channel to use this command.")
             return
 
-        channel = ctx.author.voice.channel
-        voice = await channel.connect()
-        await ctx.send(f"Connecting to {channel.name}.")
+        if ctx.voice_client is None:
+            await ctx.send('Make me join a voice channel first.')
+            return
 
-        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        with youtube_dl.YoutubeDL(self.YDL_OPTIONS) as ydl:
             info = ydl.extract_info(youtube_url, download=False)
             url = info['formats'][0]['url']
 
             source = discord.FFmpegPCMAudio(url)
-            player = voice.play(source)
+            player = ctx.voice_client.play(source)
 
     @commands.command()
     async def pause(self, ctx):
@@ -54,3 +65,4 @@ class MusicPlayer(commands.Cog):
     async def resume(self, ctx):
         ctx.voice_client.resume()
         await ctx.send("Resuming.")
+
